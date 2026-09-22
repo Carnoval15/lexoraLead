ee464fe760f59a9261da4910a8d8358cb2ab9d43
carnoval15 <carnoval15@protonmail.com>
2026-09-22 10:56:17 +0330

    connection to layerbase db is now pooled

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,6 +2,13 @@ import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import { DATABASE_URL } from '$env/static/private';
 
-const connection = await mysql.createConnection(DATABASE_URL);
+const pool = mysql.createPool({
+  uri: DATABASE_URL,
+  connectTimeout: 30000,     // 30 seconds (needed for Layerbase free tier)
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
+  enableKeepAlive: true
+});
 
-export const db = drizzle(connection);
+export const db = drizzle(pool);
